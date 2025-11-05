d7abedd0670e59031e9b09e9ef3d5989c4d3c3ce
Sarvesh Kashyap <sarbeswar.adk@gmail.com>
2025-11-05 18:48:16 +0545

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { assets } from "@/assets/assets";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 gap-10 py-14 border-b border-gray-500/30 text-gray-500">
+        <div className="w-4/5">
+          <Link href="/" className="cursor-pointer text-2xl font-extrabold tracking-tight inline-block">
+            ShopSense
+          </Link>
+          <p className="mt-6 text-sm">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+        </div>
+
+        <div className="w-1/2 flex items-center justify-start md:justify-center">
+          <div>
+            <h2 className="font-medium text-gray-900 mb-5">Company</h2>
+            <ul className="text-sm space-y-2">
+              <li>
+                <a className="hover:underline transition" href="#">Home</a>
+              </li>
+              <li>
+                <a className="hover:underline transition" href="#">About us</a>
+              </li>
+              <li>
+                <a className="hover:underline transition" href="#">Contact us</a>
+              </li>
+              <li>
+                <a className="hover:underline transition" href="#">Privacy policy</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="w-1/2 flex items-start justify-start md:justify-center">
+          <div>
+            <h2 className="font-medium text-gray-900 mb-5">Get in touch</h2>
+            <div className="text-sm space-y-2">
+              <p>+1-234-567-890</p>
+              <p>contact@shopsense.dev</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="py-4 text-center text-xs md:text-sm">
+        Build with ❤️ by{" "}
+        <Link
+          href="https://sarveshadk.xyz"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline"
+        >
+          @sarveshadk
+        </Link>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
